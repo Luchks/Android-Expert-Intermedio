8cba75b08e2356b99c2a5ef8fadfeca2856ff50a
Luchks <luis.nazario.loyola.aquino@gmail.com>
2024-03-26 12:47:57 -0500

    first commit

---
+++ app/src/main/java/com/example/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscapp.R
+import com.example.horoscapp.R.id.fragmentContainerView
+import com.example.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+       initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost= supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
